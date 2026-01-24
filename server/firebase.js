9597048ec3ebc458f8ad1eca8c8ebb77995e5275
Jeeva1121 <jeevanantham1035@gmail.com>
2026-01-25 01:21:26 +0530

    Secure Firebase credentials using environment variables

--- server/firebase.js
+++ server/firebase.js
@@ -1,13 +1,17 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from 'dotenv';
+
+// Ensure env vars are loaded
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSOMyt9zZ_79MtU-Z6TWfko2ai0-dKRXY",
-    authDomain: "hackathon-be3c4.firebaseapp.com",
-    projectId: "hackathon-be3c4",
-    storageBucket: "hackathon-be3c4.appspot.com",
-    messagingSenderId: "351772888689",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
